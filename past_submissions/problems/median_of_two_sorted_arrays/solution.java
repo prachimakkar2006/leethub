83346430de11d45307fc926e6c980e8de77b266d
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-12-09 12:51:33 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (java)

---
+++ past_submissions/problems/median_of_two_sorted_arrays/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int [] merged = new int [n+m];
+        for(int i=0;i<n;i++){
+            merged[i] = nums1[i];
+        }
+        for(int i=0;i<m;i++) {
+            merged[n+i]=nums2[i];
+        }
+        Arrays.sort(merged);
+        int len = merged.length;
+        if(len%2 ==1) {
+            return merged[len/2];
+        }
+        return (merged[(len/2)-1]+merged[len/2])/2.0;
+    }
+}
