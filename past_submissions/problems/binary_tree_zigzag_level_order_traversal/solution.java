3c85a2b0272e76d6bd86279e5aa3cb066101496e
prachimakkar2006 <prachimakkar2006@gmail.com>
2026-05-09 08:13:04 +0000

    Sync LeetCode submission - Binary Tree Zigzag Level Order Traversal (java)

--- past_submissions/problems/binary_tree_zigzag_level_order_traversal/solution.java
+++ past_submissions/problems/binary_tree_zigzag_level_order_traversal/solution.java
@@ -15,28 +15,25 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result =  new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>(); 
-        if(root == null) return result;
-        q.add(root);
-        boolean ltr = true;
-        while(!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> lt = new ArrayList<>();
-            for(int i=0;i<size;i++) {
-                TreeNode curr = q.remove();
-                if(ltr) {
-                    lt.add(curr.val);          
-                } else {
-                    lt.add(0, curr.val);     
-                }
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-            }
-            ltr = !ltr; 
-            result.add(lt);
+    List<List<Integer>> result = new ArrayList<>();
+    if(root == null) return result;
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    boolean ltr = true;
+    while(!q.isEmpty()) {
+        int size = q.size();
+        List<Integer> lt = new ArrayList<>();
+        for(int i=0;i<size;i++) {
+            TreeNode curr = q.remove();
+            if(ltr) lt.add(curr.val);
+            else lt.add(0,curr.val);
+            if(curr.left!=null) q.add(curr.left);
+            if(curr.right!=null) q.add(curr.right);
         }
-
-        return result;
+        ltr=!ltr;
+        result.add(lt);
+    }
+    return result;
+       
     }
 }
