4d939e686bedf3816846afc8e0ba39aba43a2df0
prachimakkar2006 <prachimakkar2006@gmail.com>
2026-07-03 15:13:20 +0000

    Sync LeetCode submission - Maximum Product Subarray (java)

--- past_submissions/problems/maximum_product_subarray/solution.java
+++ past_submissions/problems/maximum_product_subarray/solution.java
@@ -1,20 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
-      int n = nums.length;
-      int maxP = nums[0];
-      int minP = nums[0];
-      int max = nums[0];
-      for(int i=1; i<n; i++) {
-        if(nums[i] < 0) {
-            int temp = maxP;
-            maxP = minP;
-            minP = temp;
+        int n = nums.length;
+        int minP=1;
+        int maxP =1;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++) {
+            if(nums[i]<0) {
+                int temp = maxP;
+                maxP = minP;
+                minP = temp;
+            }
+            maxP = Math.max(nums[i],nums[i]*maxP);
+            minP= Math.min(nums[i],nums[i]*minP);
+            max = Math.max(max,maxP);
         }
-        maxP = Math.max(nums[i],nums[i]*maxP);
-        minP = Math.min(nums[i],nums[i]*minP);
-        max = Math.max(max,maxP);
-      }
-      return max;
+        return max;
 
+        
+        
     }
 }
