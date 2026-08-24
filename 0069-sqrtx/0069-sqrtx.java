4d98e45b4d2588fc1bcc9ae9b71e16f41704d35a
prachimakkar2006 <prachimakkar2006@gmail.com>
2026-08-24 07:19:06 +0530

    Time: 1 ms (98.43%), Space: 42.5 MB (79.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,9 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x<2) return x;
-       for(long i=1;i<x;i++) {
-        if(i*i > x) return (int)i-1;
-       }
-       return 1;
+        int start = 1;
+        int end = x;
+        int num =0;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(mid <= x/mid) {
+                num = mid;
+                start = mid+1;
+            }
+            else  end = mid-1;
+        }
+        return num;
     }
 }
